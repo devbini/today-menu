bce1863c2ef762bf78d017b279dc1c8c96e14a58
devbini <cong8685@naver.com>
2024-08-16 13:13:37 +0900

    docs: Add page.tsx description 2024-08-16 페이지 설명 추가

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,3 +1,17 @@
+/**************************************
+
+  # 구내식당 메뉴 미리보기 서비스 #
+  # 작성자 : 김찬빈 (Kim Chan Been, https://github.com/devbini)
+  # 코드 작성 날짜 (업데이트 날짜) : 2024-08-16
+
+  # 파일 역할
+  # 1. 사용자가 마주하는 첫 화면
+  # 2. 기타 요소들을 한번에 로드하는 곳
+
+***************************************/
+
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
