d061cebcefd651f9ba317d60daa88e713fbac2d2
devbini <cong8685@naver.com>
2025-03-04 00:23:00 +0900

    feat: 리뷰 읽기 기능 (예) 추가

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -22,6 +22,7 @@ import Contectmenu from "./components/contect";
 import "./css/page.css";
 import "./css/adminpage.css";
 import "./css/contect.css";
+import ReviewPopup from "./components/reviewpage";
 
 interface Data {
   url: string;
@@ -29,10 +30,17 @@ interface Data {
   side: string;
 }
 
+interface Review {
+  message: string;
+  date: string;
+  rate: number;
+}
+
 export default function Home() {
   // 데이터 저장 및 에러 내용 저장
   const [server_data, setData] = useState<Data | undefined>();
   const [error, setError] = useState<string | undefined>();
+  const [review_data, setReview_Data] = useState<Review[]>([]);
 
   // 이미지 URL 상태
   const [imageUrl, setImageUrl] = useState<string | undefined>();
@@ -40,6 +48,11 @@ export default function Home() {
   // 관리자 전용 팝업 ON / OFF용 변수&함수
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
 
+  // 리뷰 전용 팝업
+  const [isReviewPopupOpen, setIsReviewPopupOpen] = useState<Boolean>(false);
+
+  // 입력 리뷰
+  const [message, setmessage] = useState("");
   const [rating, setRating] = useState(5);
 
   const handlePopupOpen = () => {
@@ -48,6 +61,14 @@ export default function Home() {
 
   const handlePopupClose = () => {
     setIsPopupOpen(false);
+  };
+
+  const handleReviewPopupOpen = () => {
+    setIsReviewPopupOpen(true);
+  };
+
+  const handleReviewPopupClose = () => {
+    setIsReviewPopupOpen(false);
   };
 
   // 로딩 화면
@@ -69,9 +90,21 @@ export default function Home() {
       });
   };
 
+  const getReviewData = () => {
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/getreviews")
+      .then((response) => response.json())
+      .then((data: Review[]) => {
+        setReview_Data(data);
+      })
+      .catch((error) => {
+        console.error("리뷰 데이터 가져오기 오류:", error);
+      });
+  };
+
   // 페이지 로드시 데이터 가져오기
   useEffect(() => {
     fetchData();
+    getReviewData();
   }, []);
 
   // 값이 정상적으로 들어왔는지 확인합니다.
@@ -123,11 +156,6 @@ export default function Home() {
           <button className="admin-button" onClick={handlePopupOpen}>
             Staff Only
           </button>
-
-          {/* 관리자 전용 팝업 페이지 제공 */}
-          {isPopupOpen && (
-            <Adminpage onClose={handlePopupClose} onUploadSuccess={fetchData} />
-          )}
         </div>
 
         {/* 리뷰 페이지 */}
@@ -149,28 +177,61 @@ export default function Home() {
 
           {/* 리뷰 작성 인풋 */}
           <div className="review-input">
-            <input type="text" placeholder="리뷰를 입력하세요!" />
-            <button>전송</button>
+            <input
+              value={message}
+              onChange={(e) => setmessage(e.target.value)}
+              type="text"
+              placeholder="리뷰를 입력하세요!"
+            />
+            <button onClick={handleReviewPopupOpen}>전송</button>
           </div>
 
           {/* 리뷰 목록 */}
           <ul className="review-list">
-            <li className="review-item" key="1">
-              <div className="review-content">
-                <div className="review-rating">
-                  {Array.from({ length: 5 }, (_, i) => (
-                    <span key={i} style={{ color: i < 3 ? "#ffc107" : "#ccc" }}>
-                      ★
-                    </span>
-                  ))}
-                </div>
-                맛있었어요.
-              </div>
-              <div className="review-date">2025-03-01 13:12</div>
-            </li>
+            {review_data &&
+              review_data.map(
+                (
+                  review: { message: string; date: string; rate: number },
+                  index: number
+                ) => (
+                  <li className="review-item" key={index}>
+                    <div className="review-content">
+                      <div className="review-rating">
+                        {Array.from({ length: 5 }, (_, i) => (
+                          <span
+                            key={i}
+                            style={{
+                              color: i < review.rate ? "#ffc107" : "#ccc",
+                            }}
+                          >
+                            ★
+                          </span>
+                        ))}
+                      </div>
+                      {review.message}
+                    </div>
+                    <div className="review-date">{review.date}</div>
+                  </li>
+                )
+              )}
           </ul>
         </div>
       </div>
+
+      {/* 관리자 전용 팝업 페이지 제공 */}
+      {isPopupOpen && (
+        <Adminpage onClose={handlePopupClose} onUploadSuccess={fetchData} />
+      )}
+
+      {/* 리뷰 팝업 페이지 제공 */}
+      {isReviewPopupOpen && (
+        <ReviewPopup
+          onClose={handleReviewPopupClose}
+          onUploadSuccess={getReviewData}
+          reviewMessage={message}
+          reviewRating={rating}
+        />
+      )}
     </>
   );
 }
