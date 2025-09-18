130ca17576b6de37a21cafe33e890781ab6d75b9
devbini <cong8685@naver.com>
2025-09-18 10:55:30 +0900

    hotfix: dotenv 구동 조건 설정 및 충돌 수정
    Dotenv는 Azure Container 내에서 설치되지 않음.

--- Back/routes/api.js
+++ Back/routes/api.js
@@ -22,8 +22,8 @@ function getTimeStamp() {
 function getTimeStamp() {
   const now = new Date();
   const yyyy = now.getFullYear();
-  const mm = String(now.getMonth() + 1).padStart(2, '0');
-  const dd = String(now.getDate()).padStart(2, '0');
+  const mm = String(now.getMonth() + 1).padStart(2, "0");
+  const dd = String(now.getDate()).padStart(2, "0");
   return `${yyyy}${mm}${dd}`;
 }
 
