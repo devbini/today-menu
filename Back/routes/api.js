9f011d88efec44e4b3d174a386942a44fc9d349b
devbini <cong8685@naver.com>
2025-09-19 00:58:36 +0900

    fix: csrf-token 보관 방식 변경
    서버 내 메모리 저장 부분 삭제

--- Back/routes/api.js
+++ Back/routes/api.js
@@ -281,10 +281,7 @@ function authenticateToken(req, res, next) {
 
 // CSRF 토큰을 세션에 저장하고 재사용
 router.get("/csrf-token", function (req, res) {
-  if (!req.session.csrfToken) {
-    req.session.csrfToken = req.csrfToken();
-  }
-  res.json({ csrfToken: req.session.csrfToken });
+  res.json({ csrfToken: req.csrfToken() });
 });
 
 module.exports = router;
