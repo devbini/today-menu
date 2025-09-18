deba63e3aac4d567c65717a54cb423a30895870d
devbini <cong8685@naver.com>
2025-09-18 13:02:12 +0900

    hotfix: Express 서버 시작 중복 에러 수정
    app.js와 www에서 동시에 시작하려는 버그 확인 및 삭제

--- Back/app.js
+++ Back/app.js
@@ -87,9 +87,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 module.exports = app;
