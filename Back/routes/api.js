2fc1697873457ea716048062cb0ff758d2fdd0ea
devbini <cong8685@naver.com>
2025-09-22 00:52:55 +0900

    hotfix: samesite error 원인분석용 로그 추가

--- Back/routes/api.js
+++ Back/routes/api.js
@@ -170,6 +170,8 @@ router.post("/uploadReview", async function (req, res, next) {
 
 // POST /api/login
 router.post("/login", async function (req, res, next) {
+  console.log('[DEBUG] API.JS v4 - FORCED REFRESH');
+
   const { id, pw } = req.body;
 
   const query = "SELECT * FROM admin_tb WHERE id = ?";
