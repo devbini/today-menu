d1d3a95af50236387d01ca638a626a84053fd0ea
devbini <cong8685@naver.com>
2025-09-22 04:08:18 +0900

    chore: 코드 포매팅 및 미사용 파일 삭제

--- Front/src/app/components/contect.tsx
+++ Front/src/app/components/contect.tsx
@@ -17,33 +17,33 @@ import { SiGoogleforms } from "react-icons/si";
 interface contectprob {}
 
 const Contect: React.FC<contectprob> = ({}) => {
-    const F_Onlick = () => {
-        window.open("https://forms.gle/tWHsLL7B8JGie18K9", "_blank");
-    };
+  const F_Onlick = () => {
+    window.open("https://forms.gle/tWHsLL7B8JGie18K9", "_blank");
+  };
 
-    const G_Onlick = () => {
-        window.open("https://github.com/devbini/today-menu", "_blank");
-    };
+  const G_Onlick = () => {
+    window.open("https://github.com/devbini/today-menu", "_blank");
+  };
 
-    return (
-        <div className="contect-root">
-            <button
-                className="contect-button"
-                onClick={() => F_Onlick()}
-                aria-label="문의하기 (새 창 열림)"
-            >
-                <SiGoogleforms size="1.75rem" />
-            </button>
+  return (
+    <div className="contect-root">
+      <button
+        className="contect-button"
+        onClick={() => F_Onlick()}
+        aria-label="문의하기 (새 창 열림)"
+      >
+        <SiGoogleforms size="1.75rem" />
+      </button>
 
-            <button
-                className="contect-button github"
-                onClick={() => G_Onlick()}
-                aria-label="GitHub 저장소 (새 창 열림)"
-            >
-                <AiFillGithub size="1.75rem" />
-            </button>
-        </div>
-    );
+      <button
+        className="contect-button github"
+        onClick={() => G_Onlick()}
+        aria-label="GitHub 저장소 (새 창 열림)"
+      >
+        <AiFillGithub size="1.75rem" />
+      </button>
+    </div>
+  );
 };
 
 export default Contect;
