36d7348e69728964ba01680c93f775cbf69a9468
devbini <cong8685@naver.com>
2025-09-22 03:26:37 +0900

    feat: 깃허브 이동 버튼 추가 및 react-icons 적용

--- Front/src/app/components/contect.tsx
+++ Front/src/app/components/contect.tsx
@@ -5,38 +5,45 @@
 
 # contect.tsx 파일 역할
 # 1. 구글 폼으로 연결하는 버튼 추가
+# 2. 레포로 연결하는 버튼 추가
 
 *********************************************************************/
 
 import React from "react";
-import Image from "next/image";
 import "../css/contect.css";
+import { AiFillGithub } from "react-icons/ai";
+import { SiGoogleforms } from "react-icons/si";
 
 interface contectprob {}
 
-// Function Component (FC)
 const Contect: React.FC<contectprob> = ({}) => {
-  const F_Onlick = () => {
-    window.open("https://forms.gle/tWHsLL7B8JGie18K9", "_blank");
-  };
+    const F_Onlick = () => {
+        window.open("https://forms.gle/tWHsLL7B8JGie18K9", "_blank");
+    };
 
-  return (
-    <div className="contect-root">
-      <button
-        className="contect-button"
-        onClick={() => F_Onlick()}
-        aria-label="문의하기 (새 창 열림)"
-      >
-        <Image
-          src="/prob/contect_img.png"
-          alt="문의하기"
-          className="contect-class"
-          width={800}
-          height={800}
-        ></Image>
-      </button>
-    </div>
-  );
+    const G_Onlick = () => {
+        window.open("https://github.com/devbini/today-menu", "_blank");
+    };
+
+    return (
+        <div className="contect-root">
+            <button
+                className="contect-button"
+                onClick={() => F_Onlick()}
+                aria-label="문의하기 (새 창 열림)"
+            >
+                <SiGoogleforms size="1.75rem" />
+            </button>
+
+            <button
+                className="contect-button github"
+                onClick={() => G_Onlick()}
+                aria-label="GitHub 저장소 (새 창 열림)"
+            >
+                <AiFillGithub size="1.75rem" />
+            </button>
+        </div>
+    );
 };
 
 export default Contect;
